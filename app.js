e7158732c9dc1f15b0fff59ab2dfcc10c26fb8c8
Lucas Lins <lucaslins.link@gmail.com>
2024-09-09 02:08:58 -0300

    adding buttons mechanic

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
-console.log(portfolio);
+console.log(projects);
 
 let section = document.getElementById("resultados-pesquisa");
 console.log(section);
 
-for(let dado of portfolio){
-    section.innerHTML += `
-    <div class="item-resultado">
-        <img src="${dado.imagem}" alt="${dado.titulo}" class="imagem-projeto">
-        <div class="info-projeto">
-            <h2>
-                <a href=${dado.link } target="_blank">${dado.titulo}</a>
-            </h2>
-            <p class="descricao-meta">${dado.descricao}</p>
+
+
+function buscaProjetos(limpar=true){
+    if(limpar==true) section.innerHTML = '';
+    for(let dado of projects){
+        section.innerHTML += `
+        <div class="item-resultado">
+            <img src="${dado.imagem}" alt="${dado.titulo}" class="imagem-projeto">
+            <div class="info-projeto">
+                <h2>
+                    <a href=${dado.link } target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+            </div>
         </div>
-    </div>
-`;
+    `;
+    }
+}
+
+function buscaPesquisas(limpar=true){
+    if(limpar==true) section.innerHTML = '';
+    for(let dado of researches){
+        section.innerHTML += `
+        <div class="item-resultado">
+            <img src="${dado.imagem}" alt="${dado.titulo}" class="imagem-projeto">
+            <div class="info-projeto">
+                <h2>
+                    <a href=${dado.link } target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+            </div>
+        </div>
+    `;
+    }
+}
+
+function buscaTudo(){
+    section.innerHTML = '';
+    buscaProjetos(false);
+    buscaPesquisas(false);
 }
--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-let portfolio = [
+let projects = [
     {
         titulo : "Echo - Unity Game",
         descricao : "Echo is a prototype horror game in development, created with Unity, that aims to provide an accessible gaming experience for blind people. The project was born from the desire to create a game where the lack of vision is not an obstacle to having fun and being scared and was inspired by a blind colleague at UFRPE. To achieve this, I am exploring game design techniques focused on audio and an accessible interface, to create an experience similar to horror games from the 5th generation of consoles for blind people.",
@@ -28,5 +28,20 @@ let portfolio = [
         descricao : "This was my first university project and my initial foray into programming. It is a 2D puzzle-platformer game developed in C using the Raylib library, created by a team of six novice programmers. In this project, I was responsible for developing the first level and contributed to the menu design.",
         link : "https://github.com/HuntingBytes/V01D",
         imagem: "images/v01d.png"
+    }
+]
+
+let researches = [
+    {
+        titulo : "Communities of Practice in Large-Scale Agile Development: A Systematic Literature Mapping",
+        descricao : "The paper explores the use of Communities of Practice (CoPs) to scale agile methods in large software development projects. Through a Systematic Literature Mapping (SLM), the study found that CoPs can be effective in promoting coordination between teams, knowledge sharing, and an open community culture. These factors are crucial for successful agile adoption in large-scale projects. This paper was written during a scientific initiation project with Marcelo Marinho from UFRPE.",
+        link : "https://ieeexplore.ieee.org/document/9959929",
+        imagem: "images/cops.jpg"
+    },
+    {
+        titulo : "Evolução do Processo de Testes do TCE-PE: resultados preliminares de um projeto de BPM",
+        descricao : "Neste trabalho, descrevemos a experiência bem-sucedida de um projeto piloto de melhoria de processos de teste de software no TCE-pE, realizado a partir de uma cooperação técnica da instituição com a UFRPE. O piloto utilizou princípios da metodologia BPM para identificar necessidades, propor mudanças e monitorar o processo de testes em um projeto de desenvolvimento de software. Relatamos não só resultados iniciais, mas principalmente lições aprendidas com esta iniciativa de parceria governo-academia.",
+        link : "https://sol.sbc.org.br/index.php/sbsi_estendido/article/view/24602",
+        imagem: "images/tcepe.jpg"
     }
 ]
