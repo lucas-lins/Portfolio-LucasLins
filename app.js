07d178eb462c0fb984270af47b496910bb343655
Lucas Lins <lucaslins.link@gmail.com>
2024-09-20 16:52:11 -0300

    Adjusting projects page

--- app.js
+++ app.js
@@ -8,12 +8,23 @@ function buscaProjetos(limpar=true){
             <img src="${dado.imagem}" alt="${dado.titulo}" class="imagem-projeto">
             <div class="info-projeto">
                 <h2>
-                    <a href=${dado.link } target="_blank">${dado.titulo}</a>
+                    <a href=${dado.github} target="_blank">${dado.titulo}</a>
                 </h2>
                 <p class="descricao-meta">${dado.descricao}</p>
             </div>
-        </div>
-    `;
+            <div class="icon-group">
+                <div class="icon-container">
+                    <a href=${dado.github} target="_blank">
+                        <i class="fab fa-github"></i>
+                    </a>
+                </div>
+                <div class="icon-container">
+                    <a href=${dado.deploy} target="_blank">
+                        <i class="fa-solid fa-link""></i>
+                    </a>
+                </div>
+            </div>
+        </div>`;
     }
 }
 
@@ -25,12 +36,23 @@ function buscaPesquisas(limpar=true){
             <img src="${dado.imagem}" alt="${dado.titulo}" class="imagem-projeto">
             <div class="info-projeto">
                 <h2>
-                    <a href=${dado.link } target="_blank">${dado.titulo}</a>
+                    <a href=${dado.link} target="_blank">${dado.titulo}</a>
                 </h2>
                 <p class="descricao-meta">${dado.descricao}</p>
             </div>
-        </div>
-    `;
+            <div class="icon-group">
+                <div class="icon-container">
+                    <a href=${dado.link} target="_blank">
+                        <i class="fa-regular fa-file"></i>
+                    </a>
+                </div>
+                <div class="icon-container">
+                    <a href=${dado.lattes} target="_blank">
+                        <i class="fas fa-graduation-cap""></i>
+                    </a>
+                </div>
+            </div>
+        </div>`;
     }
 }
 
