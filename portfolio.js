ad6ab261b8fb1dd365748c60d759839801987ec0
Lucas Lins <lucaslins.link@gmail.com>
2024-09-20 16:52:11 -0300

    Adjusting projects page

--- portfolio.js
+++ portfolio.js
@@ -2,31 +2,36 @@ let projects = [
     {
         titulo : "Echo - Unity Game",
         descricao : "Echo is a prototype horror game in development, created with Unity, that aims to provide an accessible gaming experience for blind people. The project was born from the desire to create a game where the lack of vision is not an obstacle to having fun and being scared and was inspired by a blind colleague at UFRPE. To achieve this, I am exploring game design techniques focused on audio and an accessible interface, to create an experience similar to horror games from the 5th generation of consoles for blind people.",
-        link : "https://github.com/lucas-lins/Echo",
+        github : "https://github.com/lucas-lins/Echo",
+        deploy: "",
         imagem: "images/echo.png"
     },
     {
         titulo : "Steel War - Unity Game",
         descricao : "Steel War is a 2D action shooter game prototype, developed with Unity, inspired by classics like Metal Slug and featuring point-and-click mechanics. The project was created as a group activity in the Digital Games subject at UFRPE.",
-        link : "https://github.com/VictorG-028/Unity-Steel-War-Game",
+        github : "https://github.com/VictorG-028/Unity-Steel-War-Game",
+        deploy: "https://play.unity.com/en/games/6e75f0af-36df-44f3-90ef-7159673faecd/steel-war-final-release5",
         imagem: "images/steelwar.png"
     },
     {
         titulo : "My portfolio - HTML | CSS | JS",
         descricao : "This portfolio is a work in progress that I started while learning front-end development with JavaScript, CSS, and HTML during an intensive course. I plan to keep adding to it as I learn new skills, and if you are reading this, you ate seeing the result of the project.",
-        link : "https://github.com/lucas-lins/Learning-Front-End",
+        github : "https://github.com/lucas-lins/Learning-Front-End",
+        deploy: "https://lucaslinsportfolio.vercel.app",
         imagem: "images/foto.jpg"
     },
     {
         titulo : "Cooks' Books - Java project",
         descricao : "Cooks' Books is a platform for creating and sharing personal recipe collections. Users can organize and store their recipes in digital notebooks and discover new recipes from other users. This project was built in Java for a university course.",
-        link : "https://github.com/HuntingBytes/The-Cooks-Books",
+        github : "https://github.com/HuntingBytes/The-Cooks-Books",
+        deploy: "",
         imagem: "images/cooksbook.jpg"
     },
     {
         titulo : "V01D - C/Raylib Game",
         descricao : "This was my first university project and my initial foray into programming. It is a 2D puzzle-platformer game developed in C using the Raylib library, created by a team of six novice programmers. In this project, I was responsible for developing the first level and contributed to the menu design.",
-        link : "https://github.com/HuntingBytes/V01D",
+        github : "https://github.com/HuntingBytes/V01D",
+        deploy: "https://www.youtube.com/watch?v=VIhKoEdiUeY",
         imagem: "images/v01d.png"
     }
 ]
@@ -36,12 +41,14 @@ let researches = [
         titulo : "Communities of Practice in Large-Scale Agile Development: A Systematic Literature Mapping",
         descricao : "The paper explores the use of Communities of Practice (CoPs) to scale agile methods in large software development projects. Through a Systematic Literature Mapping (SLM), the study found that CoPs can be effective in promoting coordination between teams, knowledge sharing, and an open community culture. These factors are crucial for successful agile adoption in large-scale projects. This paper was written during a scientific initiation project with Marcelo Marinho from UFRPE.",
         link : "https://ieeexplore.ieee.org/document/9959929",
+        lattes: "http://lattes.cnpq.br/2826527629990948",
         imagem: "images/cops.jpg"
     },
     {
         titulo : "Evolução do Processo de Testes do TCE-PE: resultados preliminares de um projeto de BPM",
         descricao : "Neste trabalho, descrevemos a experiência bem-sucedida de um projeto piloto de melhoria de processos de teste de software no TCE-pE, realizado a partir de uma cooperação técnica da instituição com a UFRPE. O piloto utilizou princípios da metodologia BPM para identificar necessidades, propor mudanças e monitorar o processo de testes em um projeto de desenvolvimento de software. Relatamos não só resultados iniciais, mas principalmente lições aprendidas com esta iniciativa de parceria governo-academia.",
         link : "https://sol.sbc.org.br/index.php/sbsi_estendido/article/view/24602",
+        lattes: "http://lattes.cnpq.br/2826527629990948",
         imagem: "images/tcepe.jpg"
     }
 ]
